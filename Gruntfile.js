6b8f38f0f1ee2968922e1d0659e42266ed1d3494
dylan1p <dylan.clarke@mydit.ie>
2015-03-04 11:41:53 +0000

    seed changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-// Generated on 2014-11-24 using generator-angular-fullstack 2.0.13
+// Generated on 2015-03-03 using generator-angular-fullstack 2.0.13
 'use strict';
 
 module.exports = function (grunt) {
--- server/config/seed.js
+++ server/config/seed.js
@@ -4,48 +4,9 @@
  */
 
 'use strict';
-/*
+
 var User = require('../api/user/user.model');
 var Item = require('../api/item/item.model');
-var Category = require('../api/category/category.model');
-var Swap = require('../api/swap/swap.model');*/
-/*Swap.find({}).remove(function(){});
-*/
-/*Category.find({}).remove(function() {
-  Category.create({
-    name: 'Electronics'
-  }, {
-    name: 'Motor'
-  },{
-    name: 'Furniture'
-  },{
-    name: 'Musical Instruments'
-  }, {
-    name:'Clothing'
-  }, function() {
-      console.log('finished populating Category');
-    }
-  );
-});*/
+var Swap = require('../api/swap/swap.model');
 
 
-/*Item.find({}).remove(function(){
-  console.log('items removed');
-});
-User.find({}).remove(function() {
-  User.create({
-    provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
-  }, {
-    provider: 'local',
-    role: 'admin',
-    name: 'Admin',
-    email: 'admin@admin.com',
-    password: 'admin'
-  }, function() {
-      console.log('finished populating users');
-    }
-  );
-});
